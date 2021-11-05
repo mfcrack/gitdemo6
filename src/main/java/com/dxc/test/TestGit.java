1a47725fab99785e36cab752739e396d00b6a2f0
xli65 <xiang.li@dxc.com>
2021-11-05 14:55:48 +0800

    这是lixiang01的再次提交，这个提交中在pull的时候，会有冲突，因为都修改了testgit程序

--- src/main/java/com/dxc/test/TestGit.java
+++ src/main/java/com/dxc/test/TestGit.java
@@ -4,5 +4,7 @@ public class TestGit {
 
     public static void main(String[] args) {
         System.out.println("this is the base version");
+        System.out.println("想还修改了这个程序，但是这个地方其实被zhenyu修改了");
+        System.out.println("在家一行了呢");
     }
 }
---
+++ src/main/java/com/dxc/test/TestXiang2.java
@@ -0,0 +1,8 @@
+package com.dxc.test;
+
+public class TestXiang2 {
+
+    public static void main(String[] args) {
+        System.out.println("xiang增加了一个java程序");
+    }
+}
